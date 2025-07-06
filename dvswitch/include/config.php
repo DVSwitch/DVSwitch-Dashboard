c93cc7f527a2a6f453562262083d02b5fd1e38b5
Steve Zingman <N4IRS@users.noreply.github.com>
2025-07-06 16:47:39 -0400

    Fix Typo

--- dvswitch/include/config.php
+++ dvswitch/include/config.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 define("ABINFO","31001");
 // Put remote Network or IP address from which want to see tooltip ABInfo
-// IP address has mast /32 for netoworks use /24 etc
+// IP address has mast /32 for networks use /24 etc
 define("REMOTENET", "127.0.0.1/32");
 // RX Monitor YES = enabled NO = disabled
 define("RXMONITOR","YES");
