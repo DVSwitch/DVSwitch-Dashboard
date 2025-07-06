c9354fcccba48e0a90e30f9576e36b2ddeb71132
Steve Zingman <N4IRS@users.noreply.github.com>
2025-07-06 16:56:26 -0400

    Make version number visable

--- dvswitch/index.php
+++ dvswitch/index.php
@@ -117,7 +117,7 @@ $mmdvmconfigfile = getMMDVMConfigFileContent();
     echo '</div>'."\n";
 ?>
 <div class="content">
-<center><span style="font: 7pt arial, sans-serif;">DVSwitch Dashboard <?php $cdate=date("Y"); if ($cdate > "2020") {$cdate="2020-".date("Y");} echo $cdate; ?>
+<center><span style="font: 7pt arial, sans-serif;">DVSwitch Dashboard Version 20250706 <?php $cdate=date("Y"); if ($cdate > "2020") {$cdate="2020-".date("Y");} echo $cdate; ?>
 	<br>Dashboard based on Pi-Star Dashboard, © Andy Taylor (MW0MWZ) and adapted to DVSwitch by SP2ONG</span></center>
 <!-- DVSwitch Dashboard: version 20250101 -->
 	</div>
